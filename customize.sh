e2b352232da29877fb443d470344db965594e483
vincent <vincent0214@qq.com>
2021-03-06 09:49:46 +0800

    fix bug

--- customize.sh
+++ customize.sh
@@ -9,4 +9,4 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Update argon theme
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && rm -r package/lean/luci-theme-argon && mv luci-theme-argon package/lean/
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && rm -r openwrt/package/lean/luci-theme-argon && mv luci-theme-argon openwrt/package/lean/
