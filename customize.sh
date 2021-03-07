62e8c7c5f5f8ee87bbb6da32e36ec7dd283ef2cd
vincent <vincent0214@qq.com>
2021-03-07 15:50:10 +0800

    lan口ip改为123网段

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Update argon theme
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && rm -r openwrt/package/lean/luci-theme-argon && mv luci-theme-argon openwrt/package/lean/
